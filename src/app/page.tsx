e195eae5ac46d66ea79d7dcfd51afd2a42f3cd90
Hoadz20 <dohuuhoa05@gmail.com>
2025-02-26 01:04:29 +0700

    start of body + finish head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,36 @@
 
+import { Color } from "antd/es/color-picker";
+import getFontSizes from "antd/es/theme/themes/shared/genFontSizes";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <div className="h-24 bg-white">
+    <div className="flex min-h-screen flex-col ">
+      <div className="sticky z-50 top-0 h-24 bg-white">
         <div className="flex px-3 h-full "> 
           <div className="flex h-full items-center ml-10">
-          <Image className="" src="/asset/dfa01b28-eb80-431a-baef-8bedd46c1fd0-web-logo.png" alt="" width={148} height={48}/>
+          <Image className="" src="/asset/dfa01b28-eb80-431a-baef-8bedd46c1fd0-web-logo.png" alt="" width={125} height={40} />
           </div>
           <div className="flex h-full gap-10 flex-1 justify-end  ">
-            <div className="flex items-center">
-              <a className="text-black font-semibold" href="">Trang chủ</a>
+            <div className="flex items-center ">
+              <a className="text-black font-semibold hover:text-green-600" href="">Trang chủ</a>
             </div>
             <div className="flex items-center">
-              <a className="text-black font-semibold" href="">Về chúng tôi</a>
+              <a className="text-black font-semibold hover:text-green-600" href="">Về chúng tôi</a>
             </div>
             <div className="flex items-center">
-              <a className="text-black font-semibold" href="">Khóa học</a>
+              <a className="text-black font-semibold hover:text-green-600" href="">Khóa học</a>
             </div>
             <div className="flex items-center">
-              <a className="text-black font-semibold" href="">Liên hệ</a>
+              <a className="text-black font-semibold hover:text-green-600" href="">Liên hệ</a>
             </div>
             <div className="flex items-center">
-              <a className="text-black font-semibold" href="">Tin tức</a>
+              <a className="text-black font-semibold hover:text-green-600" href="">Tin tức</a>
             </div>
             <div className="flex items-center">
               <div className="">
-                <a className="text-black font-semibold" href="">Thư viện</a>
+                <a className="text-black font-semibold hover:text-green-600" href="">Thư viện</a>
               </div>
               <div className="">
               <svg className="h-3 ml-2 size-6 text-black font-semibold" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -47,7 +50,39 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="flex-grow"> 
+        <div className="mb-20">
+          <div>
+          <div className="h-[600px]">
+            <div className="relative h-full w-full">
+              <Image className= "absolute h-full w-full object-cover" src="/asset/5e1056d2-9b21-4237-9775-c62886cc9657-home-banner.webp" alt="" width={1920} height={1080} loading = "lazy" decoding = "async" data-nimg="1" />
+            <div className=" absolute inset-0 bg-gray-700 opacity-40"></div>
+            <div className="flex relative h-full w-full flex-col justify-center md:px-16 ">
+              <div className="prose font-semibold text-white">
+                <p className="pb-6">
+                <span className="text-4xl">
+                  <strong>
+                  <span className="text-white font-bold">GECKO - English & more</span>
+                  </strong>
+                </span>
+                </p>
+                <p>
+                  <span className="text-[11pt] font-thin ">
+                  Chuyên đào tạo các khóa học tiếng Anh dành cho trẻ em,luyện thi <br/> IELTS...
+                  </span>
+                </p>
+              </div>
+              <div className="mt-8">
+                <a className="rounded-full border border-white px-12 py-2.5 text-white hover:border-green-600 hover:bg-green-600 hover:text-white text-[9pt]" href="">
+                Liên hệ
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+         </div>
+         </div>
       <div></div>
     </div>
   );
